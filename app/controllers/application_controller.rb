88124fd1b7ce60f1b5462a64559b69e82fb98bbc
sanaevvv <sanaevvv@gmail.com>
2020-11-11 13:21:55 +0900

    シェアハウス口コミサイト 2020.11.11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+     before_action :configure_permitted_paramaters, if: :devise_controller?
+
+def after_sign_up_path_for(resource)
+     rooms_path
+end
+
+def after_sign_in_path_for(resource)
+    rooms_path
+end
+
+protected
+     def configure_permitted_paramaters
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Room.create([
+          {name: "シェアハウス渋谷", price: 100000, description: "渋谷駅より徒歩10分"},
+          {name: "シェアハウス東京", price: 100000, description: "東京駅より徒歩8分"},
+          {name: "シェアハウス新宿", price: 100000, description: "新宿駅より徒歩6分"},
+          {name: "シェアハウス赤坂", price: 100000, description: "赤坂駅より徒歩5分、赤坂見附駅より徒歩７分"},
+          {name: "シェアハウス品川", price: 100000, description: "品川駅より徒歩10分"}
+          ])
