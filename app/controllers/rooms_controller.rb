1062b990485010db92f2291a37cc194c2486db6c
sanaevvv <sanaevvv@gmail.com>
2020-11-21 14:37:41 +0900

    検索機能追加 gem :ransak 2020.11.21

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,11 @@
 class RoomsController < ApplicationController
   def index
-    @rooms = Room.all
+    # @rooms = Room.all
+
+    # 検索フォームの入力内容で検索する
+    @q = Room.ransack(params[:q])
+    # 重複を排除
+    @rooms = @q.result(distinct: true)
   end
 
   def show
