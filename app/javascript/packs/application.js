e9a83ab034fcd96422ce8f71fc52ae87799adbba
sanaevvv <sanaevvv@gmail.com>
2021-01-22 15:29:34 +0900

    スワイパー修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,9 @@ require("channels")
 
 require("packs/users")
 
+//= require swiper/swiper-bundle.js
+//= require swiper.js
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
