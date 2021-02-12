15785344c4ad3a8d501abebab67b6617f65c0e6c
sanaevvv <sanaevvv@gmail.com>
2021-02-12 15:10:14 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -123,6 +123,8 @@ ActiveRecord::Schema.define(version: 2021_01_26_030326) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "picture"
     t.integer "maxprice"
+    t.bigint "management_company_id", null: false
+    t.index ["management_company_id"], name: "index_rooms_on_management_company_id"
   end
 
   create_table "taggings", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -148,6 +150,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_030326) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -159,7 +162,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_030326) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin", default: false, null: false, comment: "true: 管理者, false: 一般ユーザー"
+    t.boolean "admin", default: false, null: false
     t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
